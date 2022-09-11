9fc75f36b17e66dec0bb033d0aaf0eb3f9f88ad7
Henrique Greghi <henriquevsg@gmail.com>
2022-09-11 18:28:59 -0300

    add

---
+++ Uri-2557/Calculo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uri_2557
+{
+    class Calculo
+    {
+        public string Valor1 { get; set; }
+        public string Valor2 { get; set; }
+        public string Valor3 { get; set; }
+
+        public void Valor(string Valor1, string Valor2, string Valor3)
+        {
+            if (Valor3 == "J")
+            {
+                int R = int.Parse(Valor1);
+                int L = int.Parse(Valor2);
+
+                int J = R + L;
+                Console.WriteLine(J);
+
+            }
+            else if (Valor2 == "L")
+            {
+                int R = int.Parse(Valor1);
+                int J = int.Parse(Valor3);
+
+                int L = R - J;
+                Console.WriteLine(L);
+
+            }
+            if (Valor1 == "R")
+            {
+                int J = int.Parse(Valor3);
+                int L = int.Parse(Valor2);
+
+                int R = J - L;
+                Console.WriteLine(R);
+
+            }
+        }
+    }
+}
---
+++ Uri-2557/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Uri_2557
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com  quantidade de teste: ");
+            int teste = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < teste; i++)
+            {
+                Console.WriteLine( "Digite o teste:");
+                string[] testes = Console.ReadLine().Split("+");
+                string valor1 = (testes[0]);
+                string[] valores = testes[1].Split("=");
+                string valor2 = valores[0];
+                string valor3 = valores[1];
+
+                Calculo calc = new Calculo();
+                calc.Valor(valor1,valor2,valor3);
+
+            }
+
+        }
+    }
+}
